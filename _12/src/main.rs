d2305fcf06ca69e50205fe80ebaca7c18397ac7b
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-04-14 18:38:47 -0600

    optimize 12

--- _12/src/main.rs
+++ _12/src/main.rs
@@ -1,8 +1,28 @@
 use arun_euler_lib::triangular::Triangular;
+
+fn num_divisors(n: usize) -> usize {
+    let mut ret = 1;
+
+    let mut n = n;
+
+    let mut factor = 2;
+    while factor <= n {
+        let mut tmp = 1;
+        while n % factor == 0 {
+            n /= factor;
+            tmp += 1;
+        }
+        ret *= tmp;
+        factor += 1;
+    }
+
+    ret
+}
+
 fn main() {
     let x = Triangular::new()
         .find(|&x| {
-            let numdivisible = (1..=x).filter(|y| x % y == 0).count();
+            let numdivisible = num_divisors(x);
 
             500 < numdivisible
         })
